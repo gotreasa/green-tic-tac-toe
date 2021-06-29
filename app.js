35c6ffe607e5319d3bc0fbe2d45bd0b8ac1ce92c
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2021-06-29 21:43:33 +0100

    refactor: rename the API path

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
-app.get('/api/v1/dummy', (_, response) => (response.status(200).json()));
+app.get('/api/TicTacToe/newGame', (_, response) => (response.status(200).json()));
 
 const server = app.listen('9080', () => {
   console.log(`🚀 Template NodeJS app listening at http://localhost:${server.address().port}`);

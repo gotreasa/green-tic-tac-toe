f98b2098d2b56760365618284f0fb3747934d391
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2021-06-29 22:59:35 +0100

    feat: test for should return a winning vertical line for player X

--- test/ticTacToe.api.spec.js
+++ test/ticTacToe.api.spec.js
@@ -1,5 +1,8 @@
-const { BOTTOM_RIGHT, TOP_LEFT } = require('../src/constants');
+const {
+  BOTTOM_RIGHT, TOP_LEFT,
+} = require('../src/constants');
 const { Game } = require('../src/ticTacToe');
+const { newGame } = require('../src/newGame.controller');
 
 describe('API Scenario 1: Player 1 wins with a vertical line', () => {
   test('should get initial move with empty grid', () => {
@@ -38,6 +41,98 @@ describe('API Scenario 1: Player 1 wins with a vertical line', () => {
         bottomCentre: ' ',
         bottomRight: 'O',
       },
+    });
+  });
+
+  test('should return a winning vertical line for player X', () => {
+    expect(newGame()).toBe({
+      result: 'X',
+      moves: [(
+        {
+          player: ' ',
+          grid: {
+            topLeft: ' ',
+            topCentre: ' ',
+            topRight: ' ',
+            middleLeft: ' ',
+            middleCentre: ' ',
+            middleRight: ' ',
+            bottomLeft: ' ',
+            bottomCentre: ' ',
+            bottomRight: ' ',
+          },
+        },
+        {
+          player: 'X',
+          grid: {
+            topLeft: ' ',
+            topCentre: ' ',
+            topRight: ' ',
+            middleLeft: ' ',
+            middleCentre: ' ',
+            middleRight: ' ',
+            bottomLeft: 'X',
+            bottomCentre: ' ',
+            bottomRight: ' ',
+          },
+        },
+        {
+          player: 'O',
+          grid: {
+            topLeft: ' ',
+            topCentre: ' ',
+            topRight: ' ',
+            middleLeft: ' ',
+            middleCentre: 'O',
+            middleRight: ' ',
+            bottomLeft: 'X',
+            bottomCentre: ' ',
+            bottomRight: ' ',
+          },
+        },
+        {
+          player: 'X',
+          grid: {
+            topLeft: 'X',
+            topCentre: ' ',
+            topRight: ' ',
+            middleLeft: ' ',
+            middleCentre: 'O',
+            middleRight: ' ',
+            bottomLeft: 'X',
+            bottomCentre: ' ',
+            bottomRight: ' ',
+          },
+        },
+        {
+          player: 'O',
+          grid: {
+            topLeft: 'X',
+            topCentre: ' ',
+            topRight: ' ',
+            middleLeft: ' ',
+            middleCentre: 'O',
+            middleRight: ' ',
+            bottomLeft: 'X',
+            bottomCentre: ' ',
+            bottomRight: 'O',
+          },
+        },
+        {
+          player: 'X',
+          grid: {
+            topLeft: 'X',
+            topCentre: ' ',
+            topRight: ' ',
+            middleLeft: 'X',
+            middleCentre: 'O',
+            middleRight: ' ',
+            bottomLeft: 'X',
+            bottomCentre: ' ',
+            bottomRight: 'O',
+          },
+        }
+      )],
     });
   });
 });
